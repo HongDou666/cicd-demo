05e67b6092b23f338b26ac3d30d4311e5c57a1fc
zqc <Z_13937371795@163.com>
2024-08-01 21:05:29 +0800

    feat: 自动部署测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,10 +11,6 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
   </div>
 
   <p>
