5911f74da162b0ddbd1a1355150b6bde8e9b9247
晚风 <z_13937371795@163.com>
2025-08-23 22:27:11 +0800

    自动化部署

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <h1>cicd-demo</h1>
+    <h1>cicd-demo 自动化部署</h1>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
